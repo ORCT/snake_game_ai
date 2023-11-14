6fe1bec46a5bfa0aba552adc584dad438ad29ec0
ORCT <sonic407@naver.com>
2023-11-14 21:30:43 +0900

    add continuous square move, game over

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 ## 2023.11.02
 made simple type of snake game
+
+## 2023.11.13
+made move square sample code
+
+## 2023.11.14
+add continyous square move, gameOver
--- tttest.js
+++ tttest.js
@@ -1,45 +1,60 @@
 const canvas = document.getElementById('board')
 const context = canvas.getContext('2d')
 const square = {
-	x : 50,
-	y : 50,
-	size : 50,
-	speed : 50
+	x : 200,
+	y : 500,
+	size : 25,
+	speed : 25,
+	direction : 'ArrowRight'
 }
+const UPDATE_INTERVAL = 250
 
 function drawSquare() {
 	context.clearRect(0, 0, canvas.width, canvas.height)
 	context.fillStyle = '#EE9977'
-	context.fillRect(square.x, square.y, square.size, square.speed)
+	context.fillRect(square.x, square.y, square.size, square.size)
 }
 
 function moveSquare(direction) {
-	switch (direction) {
-		case 'ArrowLeft':
-			square.x -= square.speed
-			break
-		case 'ArrowUp':
-			square.y -= square.speed
-			break
-		case 'ArrowRight':
-			square.x += square.speed
-			break
-		case 'ArrowDown':
-			square.y += square.speed
-			break
+	if (direction === 'ArrowLeft') {
+		square.x -= square.speed
+	} else if (direction === 'ArrowUp') {
+		square.y -= square.speed
+	} else if (direction === 'ArrowRight') {
+		square.x += square.speed
+	} else if (direction === 'ArrowDown') {
+		square.y += square.speed
 	}
 	drawSquare()
 }
 
-function printKey(key) {
-	console.log(key)
+function update() {
+	moveSquare(square.direction)
+	boardOut()
+}
 
+function reset() {
+	square.x = 200
+	square.y = 500
+	direction = 'ArrowRight'
+}
+
+function gameOver() {
+	alert('Game Over!')
+	reset()
+}
+
+function boardOut() {
+	if (square.x >= canvas.width || square.x < 0 || square.y >= canvas.height || square.y < 0) {
+		gameOver()
+	}
 }
 
 window.addEventListener('keydown', event => {
 	const key = event.key
-	printKey(key)
-	moveSquare(key)
+	square.direction = key
 })
 
-drawSquare()
+setInterval(() => {
+	update()
+}, UPDATE_INTERVAL)
